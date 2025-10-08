57dfd8eb721863ca1bf82722f314fdbe80f8188c
CandelaStaccia <candela.staccia@gmail.com>
2025-10-08 10:55:04 -0300

    configuracion basica

--- src/main.js
+++ src/main.js
@@ -1,1 +1,17 @@
-console.log("UnaHur - Anti-Social net");
+const express = require("express")
+const {sequelize} = require("./db/models")
+
+const app = express()
+
+const PORT = process.env.PORT || 3001
+
+app.use(express.json())
+
+app.listen(PORT, async(error) => {
+    if(error) {
+        console.error(error.message)
+        process.exit(1)
+    }
+    console.log(`App iniciada correctamente en el puerto ${PORT}`)
+    //await sequelize.sync({force: true})
+})
