27b50a454fe1b0098a0a02638c2497e29f0b436c
CandelaStaccia <candela.staccia@gmail.com>
2025-10-11 12:03:22 -0300

    modificacion de rutas

--- src/routes/user.route.js
+++ src/routes/user.route.js
@@ -4,12 +4,15 @@ const {userControllers} = require('../controllers');
 const {validUser, validNickname,validEmail , validationSchemma, validationEmailSchema} = require ('../middlewares/user.midleware')
 const {invalidId} = require ('../middlewares/generic.midleware')
 
-router.get('/getUser/:id',invalidId, validUser, userControllers.getUsers);
+
+//ruta para traer a todos los user?
+
+router.get('/:id',invalidId, validUser, userControllers.getUsers);
 //createUser
-router.put('/updateNickName/:id',invalidId,validUser, validNickname, userControllers.updateNickName)//arreglar, hacer un midleware para que no haya un nick igual
-router.post('/seguirUsuario/:id/:idASeguir',invalidId, userControllers.followUser)// bonus
+router.put('/:id/nickname',invalidId,validUser, validNickname, userControllers.updateNickName)//arreglar, hacer un midleware para que no haya un nick igual
+router.post('/:id/follow/:idASeguir',invalidId, userControllers.followUser)// bonus
 //'/updateEmail/:id'
-router.delete('/deleteUser/:id',invalidId,validUser, userControllers.deleteUser)
+router.delete('/:id',invalidId,validUser, userControllers.deleteUser)
 
 
 module.exports =  router ;
