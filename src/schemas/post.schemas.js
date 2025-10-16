d8bf31f2bf0d7c6e87eee1c3dfac049121ff3753
Magali P Gutierrez <magali.g@bue.edu.ar>
2025-10-15 22:41:20 -0300

    post.schemas

--- src/schemas/post.schemas.js
+++ src/schemas/post.schemas.js
@@ -2,10 +2,22 @@ const joi = require('joi')
 
 const contenidoSchema= joi.object({
     texto: joi.string().trim().min(10).max(200).messages({
-        "string.empty": "El contenido no puede estar vacío",        
+        "string.empty": "El texto no puede estar vacio",      
         "string.min": "El contenido debe tener al menos 10 caracteres",
         "string.max": "El contenido debe contener como maximo {#limit} de caracteres
 })
 });
+const creationSchema = joi.object({
+    texto: joi.string().trim().required().min(10).max(200).messages({
+        "any.required" :"texto es obligatorio",
+        "string.min" : "El contenido debe contener como minimo {#limit} de caracteres",
+        "string.max" : "El contenido debe contener como maximo {#limit} de caracteres",
+        "string.empty": "El texto no puede estar vacio",
+    }),
+    username: joi.string().trim().required().messages({
+        "any.required" :"el username es obligatorio"    ,
+        "string.empty": "El username no puede estar vacio"
+    })
     
-module.exports={contenidoSchema}
+});
+module.exports={contenidoSchema, creationSchema}
