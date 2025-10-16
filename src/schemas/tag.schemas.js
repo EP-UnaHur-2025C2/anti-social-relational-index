0e4b129018e15c3c1063f36637cb567283e0612f
Magali P Gutierrez <magali.g@bue.edu.ar>
2025-10-15 21:40:29 -0300

    schemas -> tag

---
+++ src/schemas/tag.schemas.js
@@ -0,0 +1,13 @@
+const joi = require('joi')
+
+const tagSchema = joi.object({
+    nombre: joi.string().min(5).max(30).required().messages({
+        "any.required": "El nombre es obligatorio",
+        "string.empty": "El nombre no puede estar vacío",
+        "string.min": "El nombre debe tener al menos 5 caracteres",
+        "string.max": "El nombre debe contener como maximo {#limit} de caracteres
+
+    })
+});
+
+module.exports = { tagSchema }
