60e478b33b2c21ba014a2ff6f7d192a84e02e001
Iván Herold <ivanezequiel.herold@estudiantes.unahur.edu.ar>
2025-10-28 17:38:01 -0300

    cambio produccion

--- src/db/config/config.js
+++ src/db/config/config.js
@@ -16,12 +16,11 @@ module.exports = {
     dialect: "mysql"
   },
   production: {
-    dialect: "postgres",
-    username: process.env.DB_USER,
-    password: process.env.DB_PASSWORD,
-    database: process.env.DB_NAME,
-    host: "127.0.0.1",
-    port: 5432,
-    logging: false
+     use_env_variable: 'DATABASE_URL',
+  dialect: 'postgres',
+  logging: false,
+  dialectOptions: {
+    ssl: { require: true, rejectUnauthorized: false },
+  },
   }
 };
